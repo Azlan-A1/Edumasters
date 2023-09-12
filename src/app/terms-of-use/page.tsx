b1166628d222b0991c6bc6000e942ff9dfef5ab5
Brandon Bridges <brandonrbridges@outlook.com>
2023-09-12 01:10:39 +0100

    [Update] Privacy Policy, Terms of Use & ChatGPT Pages

---
+++ src/app/terms-of-use/page.tsx
@@ -0,0 +1,39 @@
+// GraphQL
+import { gql } from 'graphql-request'
+import { hygraph } from '@/lib/graphql'
+
+// Components
+import PageHeader from '@/components/PageHeader'
+
+// Packages
+import ReactMarkdown from 'react-markdown'
+
+export default async function PrivacyPolicy() {
+	const page = await getPageData()
+
+	return (
+		<div>
+			<PageHeader title={page.title} />
+
+			<div className='container'>
+				<ReactMarkdown>{page.content}</ReactMarkdown>
+			</div>
+		</div>
+	)
+}
+
+async function getPageData() {
+	const QUERY = gql`
+		query GetTermsOfUse {
+			metaPage(where: { slug: "terms-of-use" }) {
+				title
+				slug
+				content
+			}
+		}
+	`
+
+	const { metaPage } = (await hygraph.request(QUERY)) as { metaPage: any }
+
+	return metaPage
+}
